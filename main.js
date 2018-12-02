d15fc553e74d5c6b4ad49495c495409a297df7f4
Can <mca199667@gmail.com>
2018-12-02 15:11:45 +0300

    Javascript

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+var userInputInKg = document.getElementById("userInput");
+var userInputInGr = 0;
+var userInputInPo = 0;
+var userInputInOu = 0;
 
+userInputInKg.onkeyup = function(){
+    var kgValue = userInputInKg.value;
+    convertInput(kgValue);
+    displayConvertedValues();
+}
+
+function convertInput(value) {
+  userInputInGr = (value * 1000);
+  userInputInPo = (value * 2.20462262);
+  userInputInOu = (value * 35.2739619);
+}
+
+function displayConvertedValues() {
+  document.getElementById("inGrams").innerHTML = userInputInGr + " grams";
+  document.getElementById("inPounds").innerHTML = userInputInPo + " pounds";
+  document.getElementById("inOunces").innerHTML = userInputInOu + " ounces";
+}
